e99d4c9d52052f1a5f50c4b65f6eb390dfe753ee
kapitazzz <agarussky@gmail.com>
2020-09-30 19:28:34 +0300

    Add postgresql, admin-panel

--- myfirstblog/settings.py
+++ myfirstblog/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'myfirstblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myfirstblogdb',
+        'USER': 'postgres',
+        'PASSWORD': '123456blog',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -119,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/awesome_media/'
